be48506ad7a22758e48e96c9064f4dd5af635279
Robin Quintero <rohaquinlop301@gmail.com>
2024-04-30 00:56:03 -0500

    feat(rust): update algorithm
    - Update the algorithm to use the Myers diff algorithm.
    - Remove the `build_dp_memoized` function.
    - Update the `find_diffs` function to return a vector of `History` enums.
    - Update the `main` function to sort the `diffs` vector by line number and type.
    - Update the `main` function to print the lines in the correct order.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use anyhow::Result;
 use clap::Parser;
 use color_print::cprintln;
 use pluralizer::pluralize;
-use utils::{build_dp_memoized, get_file_content};
+use std::collections::BTreeMap;
+use utils::get_file_content;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -13,69 +14,107 @@ struct Cli {
     sc_path_file: std::path::PathBuf,
 }
 
-struct Line {
-    line_num: u32,
-    line_content: String,
-    color: u32,
+#[derive(Clone)]
+enum History {
+    Keep { line: String, num: u32 },
+    Insert { line: String, num: u32 },
+    Remove { line: String, num: u32 },
+    Frontier { x: i32, history: Vec<History> },
 }
 
-fn find_diffs(fr_lines: &Vec<String>, sc_lines: &Vec<String>) -> Vec<Line> {
-    let mut diffs: Vec<Line> = Vec::new();
-
-    let n = fr_lines.len();
-    let m = sc_lines.len();
-
-    let dp: Vec<Vec<i32>> = build_dp_memoized(&fr_lines, &sc_lines);
-
-    let mut i = n;
-    let mut j = m;
-
-    while i != 0 || j != 0 {
-        if i == 0 {
-            // Addition
-            diffs.push(Line {
-                line_num: j as u32,
-                line_content: sc_lines.get(j - 1).unwrap().clone(),
-                color: 3,
-            });
-            j -= 1;
-        } else if j == 0 {
-            // Deletion
-            diffs.push(Line {
-                line_num: i as u32,
-                line_content: fr_lines.get(i - 1).unwrap().clone(),
-                color: 2,
-            });
-            i -= 1;
-        } else if fr_lines[i - 1] == sc_lines[j - 1] {
-            // Common
-            diffs.push(Line {
-                line_num: i as u32,
-                line_content: fr_lines.get(i - 1).unwrap().clone(),
-                color: 1,
-            });
-            i -= 1;
-            j -= 1;
-        } else if dp[i - 1][j] <= dp[i][j - 1] {
-            // Addition
-            diffs.push(Line {
-                line_num: j as u32,
-                line_content: sc_lines.get(j - 1).unwrap().clone(),
-                color: 3,
-            });
-            j -= 1;
-        } else {
-            // Delete
-            diffs.push(Line {
-                line_num: i as u32,
-                line_content: fr_lines.get(i - 1).unwrap().clone(),
-                color: 2,
-            });
-            i -= 1;
+impl History {
+    fn get_x(&self) -> i32 {
+        match self {
+            History::Frontier { x, .. } => *x,
+            _ => -1,
         }
     }
 
-    diffs.reverse();
+    fn get_history(&self) -> Vec<History> {
+        match self {
+            History::Frontier { history, .. } => history.clone(),
+            _ => vec![],
+        }
+    }
+
+    fn get_line(&self) -> u32 {
+        match self {
+            History::Keep { num, .. } => *num,
+            History::Insert { num, .. } => *num,
+            History::Remove { num, .. } => *num,
+            _ => 0,
+        }
+    }
+}
+
+fn find_diffs(fr_lines: &Vec<String>, sc_lines: &Vec<String>) -> Vec<History> {
+    let mut diffs = Vec::new();
+
+    let mut frontier: BTreeMap<i32, History> = BTreeMap::new();
+
+    frontier.insert(
+        1,
+        History::Frontier {
+            x: 0,
+            history: vec![],
+        },
+    );
+
+    let fr_max = fr_lines.len() as i32;
+    let sc_max = sc_lines.len() as i32;
+
+    let mut x: i32;
+    let mut y: i32;
+    let mut history: Vec<History>;
+
+    for d in 0..(fr_max + sc_max + 1) {
+        for k in (-d..d + 1).step_by(2) {
+            let go_down =
+                if k == -d || (k != d && frontier[&(k - 1)].get_x() < frontier[&(k + 1)].get_x()) {
+                    true
+                } else {
+                    false
+                };
+
+            if go_down {
+                x = frontier[&(k + 1)].get_x();
+                history = frontier[&(k + 1)].get_history();
+            } else {
+                x = frontier[&(k - 1)].get_x() + 1;
+                history = frontier[&(k - 1)].get_history();
+            }
+
+            y = x - k;
+
+            if 1 <= y && y <= sc_max && go_down {
+                history.push(History::Insert {
+                    line: sc_lines[(y - 1) as usize].clone(),
+                    num: y as u32,
+                });
+            } else if 1 <= x && x <= fr_max {
+                history.push(History::Remove {
+                    line: fr_lines[(x - 1) as usize].clone(),
+                    num: x as u32,
+                });
+            }
+
+            while x < fr_max && y < sc_max && fr_lines[x as usize] == sc_lines[y as usize] {
+                x += 1;
+                y += 1;
+                history.push(History::Keep {
+                    line: fr_lines[(x - 1) as usize].clone(),
+                    num: x as u32,
+                });
+            }
+
+            if x >= fr_max && y >= sc_max {
+                diffs = history;
+                return diffs;
+            } else {
+                frontier.insert(k, History::Frontier { x, history });
+            }
+        }
+    }
 
     diffs
 }
@@ -91,50 +130,55 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let fr_lines = get_file_content(&args.fr_path_file)?;
     let sc_lines = get_file_content(&args.sc_path_file)?;
 
-    let diffs = find_diffs(&fr_lines, &sc_lines);
+    let mut diffs = find_diffs(&fr_lines, &sc_lines);
     let width = ((diffs.len() as f64).log10().floor() + 1.0) as usize;
     let mut total_add: u32 = 0;
     let mut total_del: u32 = 0;
 
+    // Sort the diffs by line number and by type
+    diffs.sort_by_key(|history: &History| history.get_line());
+
     for line in diffs {
-        match line.color {
-            1 => cprintln!(
-                "{line_num:>width$}: {line_content:}",
-                line_num = line.line_num,
-                width = width,
-                line_content = line.line_content,
-            ),
-            2 => {
-                total_del += 1;
-                if line.line_content.trim().is_empty() {
-                    cprintln!(
-                        "{line_num:>width$}: <red>- \\n</>",
-                        line_num = line.line_num,
-                        width = width
-                    );
-                } else {
-                    cprintln!(
-                        "{line_num:>width$}: <red>- {line_content:}</>",
-                        line_num = line.line_num,
-                        width = width,
-                        line_content = line.line_content,
-                    );
-                }
+        match line {
+            History::Keep { line, num } => {
+                cprintln!(
+                    "{line_num:>width$}: {line_content:}",
+                    line_num = num,
+                    width = width,
+                    line_content = line,
+                );
             }
-            3 => {
+            History::Insert { line, num } => {
                 total_add += 1;
-                if line.line_content.trim().is_empty() {
+                if line.trim().is_empty() {
                     cprintln!(
                         "{line_num:>width$}: <green>+ \\n</>",
-                        line_num = line.line_num,
+                        line_num = num,
                         width = width,
                     );
                 } else {
                     cprintln!(
                         "{line_num:>width$}: <green>+ {line_content:}</>",
-                        line_num = line.line_num,
+                        line_num = num,
                         width = width,
-                        line_content = line.line_content,
+                        line_content = line,
+                    );
+                }
+            }
+            History::Remove { line, num } => {
+                total_del += 1;
+                if line.trim().is_empty() {
+                    cprintln!(
+                        "{line_num:>width$}: <red>- \\n</>",
+                        line_num = num,
+                        width = width
+                    );
+                } else {
+                    cprintln!(
+                        "{line_num:>width$}: <red>- {line_content:}</>",
+                        line_num = num,
+                        width = width,
+                        line_content = line,
                     );
                 }
             }
--- src/utils.rs
+++ src/utils.rs
@@ -18,23 +18,3 @@ pub fn get_file_content(
 
     Ok(lines)
 }
-
-pub fn build_dp_memoized(fr_lines: &Vec<String>, sc_lines: &Vec<String>) -> Vec<Vec<i32>> {
-    let n = fr_lines.len() + 1;
-    let m = sc_lines.len() + 1;
-    let mut dp: Vec<Vec<i32>> = vec![vec![-1; m]; n];
-
-    for i in 0..n {
-        for j in 0..m {
-            if i == 0 || j == 0 {
-                dp[i][j] = 0;
-            } else if fr_lines[i - 1] == sc_lines[j - 1] {
-                dp[i][j] = 1 + dp[i - 1][j - 1];
-            } else {
-                dp[i][j] = std::cmp::max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
-    }
-
-    dp
-}
